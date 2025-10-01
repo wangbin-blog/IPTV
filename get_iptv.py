7ad498ebb1b85a98a18370645df2a89d4a0b3988
plplpopp <94629194+plplpopp@users.noreply.github.com>
2025-10-01 15:56:37 +0800

    Update get_iptv.py

--- get_iptv.py
+++ get_iptv.py
@@ -13,11 +13,17 @@ class IPTVManager:
     def __init__(self):
         # 配置文件
         self.SOURCE_URLS = [
-            "https://raw.githubusercontent.com/zwc456baby/iptv_alive/master/live.txt",
-            "https://live.zbds.top/tv/iptv6.txt",
-            "https://live.zbds.top/tv/iptv4.txt",
-            "http://home.jundie.top:81/top/tvbox.txt",
-            "https://mirror.ghproxy.com/https://raw.githubusercontent.com/YanG-1989/m3u/main/Gather.m3u",
+    "https://raw.githubusercontent.com/zwc456baby/iptv_alive/master/live.txt",
+    "https://raw.githubusercontent.com/iptv-org/iptv/gh-pages/countries/cn.m3u",
+    "https://ghfast.top/raw.githubusercontent.com/Supprise0901/TVBox_live/main/live.txt",
+    "https://gh-proxy.com/https://raw.githubusercontent.com/wwb521/live/main/tv.m3u",
+    "https://gh-proxy.com/https://raw.githubusercontent.com/zeee-u/lzh06/main/fl.m3u",
+    "https://raw.githubusercontent.com/Guovin/iptv-database/master/result.txt",  
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/cn.m3u",
+    "https://raw.githubusercontent.com/suxuang/myIPTV/main/ipv4.m3u",
+    "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/iptv4.txt",
+    "http://47.120.41.246:8899/zb.txt",
+    "https://live.zbds.top/tv/iptv4.txt",
         ]
         
         self.REQUEST_CONFIG = {
